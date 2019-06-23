f9c711ccadd6bc20c87fceef3680022a1ac3e2b0
DavidDF <38300010+shizukaddf@users.noreply.github.com>
2019-06-23 06:17:17 -0300

    dd

---
+++ src/app/app.module.ts
@@ -0,0 +1,104 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { IndexComponent } from './components/index/index.component';
+import { ImageComponent } from './components/image/image.component';
+import { MiembrosComponent } from './components/miembros/miembros.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { MiembroComponent } from './components/miembro/miembro.component';
+import { SlideComponent } from './components/slide/slide.component';
+import { NavColegComponent } from './components/nav-coleg/nav-coleg.component';
+import { LoginUserComponent } from './components/login-user/login-user.component';
+import { PagosComponent } from './components/pagos/pagos.component';
+import { EscribanosComponent } from './components/escribanos/escribanos.component';
+import { ContentsComponent } from './components/contents/contents.component';
+
+
+
+
+import {HttpClientModule} from '@angular/common/http';
+import { AltaEscribaniaComponent } from './components/alta-escribania/alta-escribania.component';
+import { NovedadesComponent } from './components/novedades/novedades.component';
+import { ModificarPerfilComponent } from './components/modificar-perfil/modificar-perfil.component';
+import { EscribaniasComponent } from './components/escribanias/escribanias.component';
+<<<<<<< HEAD
+import { AltaPagosComponent } from './components/alta-pagos/alta-pagos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    ImageComponent,
+    MiembrosComponent,
+    RegistroComponent,
+    AboutComponent,
+    HomeComponent,
+    RegistroComponent,
+    MiembroComponent,
+    SlideComponent,
+    NavColegComponent,
+    LoginUserComponent,
+    PagosComponent,
+    EscribanosComponent,
+    ContentsComponent,
+    AltaEscribaniaComponent,
+    NovedadesComponent,
+    ModificarPerfilComponent,
+    EscribaniasComponent,
+    AltaPagosComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+=======
+import { AltaPagosComponent } from './components/alta-pagos/alta-pagos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    ImageComponent,
+    MiembrosComponent,
+    RegistroComponent,
+    AboutComponent,
+    HomeComponent,
+    RegistroComponent,
+    MiembroComponent,
+    SlideComponent,
+    NavColegComponent,
+    LoginUserComponent,
+    PagosComponent,
+    EscribanosComponent,
+    ContentsComponent,
+    AltaEscribaniaComponent,
+    NovedadesComponent,
+    ModificarPerfilComponent,
+    EscribaniasComponent,
+    AltaPagosComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+>>>>>>> 8a454fca8c931d65548d4a8b2765abd09c041093
