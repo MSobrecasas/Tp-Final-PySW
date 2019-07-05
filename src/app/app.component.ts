6abdf3c22303dad4ad49f5be8a14ac5fc677c7dd
DavidDF <38300010+shizukaddf@users.noreply.github.com>
2019-07-04 22:01:15 -0300

    r

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import {HeaderComponent} from './components/header/header.component'
+import { HeaderComponent } from './components/header/header.component'
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,19 @@ import {HeaderComponent} from './components/header/header.component'
 })
 export class AppComponent {
   title = 'Tp-Final-PySW';
-
+  fondo: string = "container mb-1";
+  fondo2: string = "card border-0 shadow my-5"
+  fondo3: string = "card-body"
+  pantallaSM() {
+    //console.log("...")
+    if (document.documentElement.clientWidth < 700) {
+      this.fondo = 'row justify-content-center bg-white pt-5 mb-2';
+      this.fondo2 = 'col-12';
+      this.fondo3 = 'col-12';
+    } else {
+      this.fondo = 'container mb-1';
+      this.fondo2 = 'card border-0 shadow my-5';
+      this.fondo3 = 'card-body';
+    }
+  }
 }
